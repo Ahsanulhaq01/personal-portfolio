97fd5a1f1e28cf6a78c5a2c0280d1f6cc1068c71
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-10-10 17:42:30 +0500

    write Code for Otherlinks Component with its css

--- src/pages/other-links/OtherLinks.jsx
+++ src/pages/other-links/OtherLinks.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import Sidebar from '../../component/sidebar/Sidebar'
+import Sidebar from '../../component/sidebar/Sidebar';
+import './links.css'
+import { Link } from 'react-router';
 
 function OtherLinks() {
   return (
     <>
+    <div className="app-container">
+
     <Sidebar/>
+    <div className="links-container">
+      <div className="leetcode-container link">
+        <img src="/images/leetcode.jpg" alt="" />
+        <div className="link-text-container">
+          <p className='platform-name'>LeetCode</p>
+          <Link to={'https://leetcode.com/u/Ahsanulhaq01/'} target="_blank" rel="noopener noreferrer">@Ahsanulhaq01</Link>
+        </div>
+      </div>
+      <div className="github-container link">
+        <img src="/images/twitter.jpg" alt="" />
+        <div className="link-text-container">
+          <p className='platform-name'>X (twitter)</p>
+          <Link to={'https://x.com/ulhaqcdr'}  target="_blank" rel="noopener noreferrer">@ulhaqcdr</Link>
+        </div>
+      </div>
+      <div className="twitter-container link">
+        <img src="/images/linkedin.png" alt="" />
+        <div className="link-text-container">
+          <p className='platform-name'>LinkedIn</p>
+          <Link to={'https://www.linkedin.com/in/ahsan-ulhaq-8581a82ba/'}  target="_blank" rel="noopener noreferrer">@Ahsanulhaq</Link>
+        </div>
+      </div>
+      <div className="LinkedIn-container link">
+        <img src="/images/github.png" alt="" />
+        <div className="link-text-container">
+          <p className='platform-name'>GitHub</p>
+          <Link to={'https://github.com/Ahsanulhaq01'} target="_blank" rel="noopener noreferrer">@Ahsanulhaq01</Link>
+        </div>
+      </div>
+    </div>
+    </div>
+
     </>
   )
 }
