9e663e93cead1ad6db004865ea3d243fe6b2dd60
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-10-12 17:28:28 +0500

    write code for project-page component

--- src/pages/project/ProjectPage.jsx
+++ src/pages/project/ProjectPage.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import Sidebar from '../../component/sidebar/Sidebar';
-
+import { Link } from 'react-router';
+import './project-page.css'
 function ProjectPage() {
   return (
     <>
+    <div className="app-container">
     <Sidebar/>
+    <div className="projects-container">
+      <div className="project-box">
+        <div className="project-name">
+         <Link to ="/Ecommerce-With-SuperSimpleDev" className="college-name-page">Ecommerce With Simon Bao</Link>
+         <Link to ="/chatbot-with-simon" className="college-name-page">ChatBot With Simon Bao</Link>
+         <Link to ="/blog" className="college-name-page">Blog</Link>
+         <Link to ="/Ecommerce" className="college-name-page">Ecommerce</Link>
+        </div>
+      </div>
+        <div className="projects-videos">
+          <video src="/Projects-videos/ecommerce_with_superSimpleDev.mp4" controls></video>
+          <div className="link-of-source-code">
+
+          <Link to={'/'}>Source Code</Link>
+          </div>
+        </div>
+
+    </div>
+    </div>
     </>
   )
 }
