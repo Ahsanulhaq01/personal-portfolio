1b1ff1e15bbfd97c4f277ac1d2bddd3f8dbba1eb
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-10-12 17:29:02 +0500

    create separte Component for individual project

---
+++ src/pages/project/project-component/ChatBot_With_Simon.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router'
+import Sidebar from '../../../component/sidebar/Sidebar'
+import '../project-page.css'
+
+function ChatBot_With_Simon() {
+  return (
+    <>
+     <div className="app-container">
+    <Sidebar/>
+    <div className="projects-container">
+      <div className="project-box">
+        <div className="project-name">
+         <Link to ="/Ecommerce-With-SuperSimpleDev" className="college-name-page">Ecommerce With Simon Bao</Link>
+         <Link to ="/chatbot-with-simon" className="college-name-page">ChatBot With Simon Bao</Link>
+         <Link to ="/blog" className="college-name-page">Blog</Link>
+         <Link to ="/Ecommerce" className="college-name-page">Ecommerce</Link>
+        </div>
+      </div>
+        <div className="projects-videos">
+          <video src="/Projects-videos/Chatbot_with_superSimpleDev.mp4" controls></video>
+          <div className="link-of-source-code">
+
+          <Link to={'https://github.com/Ahsanulhaq01/Intermediate-React-Project/tree/main/Chatbot_in_react/'}>Source Code</Link>
+          </div>
+        </div>
+
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default ChatBot_With_Simon
