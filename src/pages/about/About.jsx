98264b130201a0704cd3aa0b6f6d13a6c2a83ce0
Ahsanulhaq01 <ulhaqahsan276@gmail.com>
2025-10-13 15:25:44 +0500

    write about page code

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,10 +1,26 @@
-import Sidebar from '../../component/sidebar/Sidebar';
+import Sidebar from "../../component/sidebar/Sidebar";
+import './about.css'
 function About() {
   return (
-   <>
-   <Sidebar/>
-   </>
-  )
+    <>
+      <div className="app-container">
+        <Sidebar />
+        <div className="about-me-container">
+            <div className="text-about-me">
+              <p className="about-me-para">
+                Hi, I’m Ahsan Ul Haq — a Software Engineering student who enjoys turning ideas into real, working digital products.
+
+I’m currently learning web development, exploring backend technologies, and improving my skills in C++, Python, and JavaScript. I’m also interested in cybersecurity and DSA to strengthen my technical foundation.
+
+My goal is to build practical, impactful projects and keep growing as a developer. I love learning new technologies and finding creative ways to solve problems.
+
+I’m always open to learning, collaboration, and new opportunities — feel free to connect with me!
+              </p>
+            </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default About
+export default About;
